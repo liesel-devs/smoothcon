b553a2cb5c57f465d2bba8b3a619d575e8277bd7
Johannes Brachem <37882800+jobrachem@users.noreply.github.com>
2026-08-12 16:40:22 +0200

    Update __init__.py

--- src/smoothcon/__init__.py
+++ src/smoothcon/__init__.py
@@ -32,4 +32,4 @@ from ._univariate import cyclic_cubic as cyclic_cubic
 from ._univariate import cyclic_pspline as cyclic_pspline
 from ._univariate import pspline as pspline
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
